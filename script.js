f459e0c7f35afb74729b9cffacf35849bceaf2fc
Shikhar Tripathi <shikhartripathi1912@gmail.com>
2023-04-26 12:50:11 +0530

    Make a five time playing game

--- script.js
+++ script.js
@@ -4,22 +4,51 @@ const getComputerChoice = () =>{
     return rps[num];
 }
 
-
+function game(){
+    let compScore=0;
+    let playerScore=0;
+    let flag =0;
+    for(let i=0;i<5;i++){
+        if(compScore>=3){
+            console.log("you lost");
+            flag=1;
+            break;
+        }
+        if(playerScore>=3){
+            console.log("you won");
+            break;
+        }
+        let choice = prompt("Rock,Paper,Scissors......Shoot");
+        let playerSelection = choice.toLowerCase();
+        let computerSelection = getComputerChoice();
+        let boo = playRound(playerSelection,computerSelection);
+        if(boo==1){
+            playerScore++;
+        }
+        else if(boo==0){
+            compScore++;
+        }
+        console.log(`you:${playerScore} and comp:${compScore}`);
+    }
+    if(flag===0){
+        if(compScore===playerScore) console.log("its a draw");
+        else if(compScore>playerScore) console.log("you lost");
+        else console.log("you won");
+    }
+}
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if(playerSelection===computerSelection){
-        return "its a draw";
+        return 2;
     }
     else if(playerSelection==="rock" && computerSelection==="scissors" ||
         playerSelection==="paper" && computerSelection==="rock" ||
         playerSelection==="scissors" && computerSelection==="paper"){
-        return "you won" ;
+        return 1;
     }
     else{
-        return "you lost";
+        return 0;
     }
   }
-  let choice = prompt("Rock,Paper,Scissors......Shoot");
-  let playerSelection = choice.toLowerCase();
-  let computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+  game();
